caaa952cf808c68f97844a9d1fadb4980371c60c
nautilus <wujindaniel1011@gmail.com>
2026-01-27 23:12:19 +0900

    Plotting

--- tests/performance/test_corr_k.py
+++ tests/performance/test_corr_k.py
@@ -20,10 +20,8 @@ DATASETS = [
 
 
 @pytest.mark.parametrize("dataset_data", DATASETS, indirect=True)
-# @pytest.mark.parametrize("n_mc", [20])
 @pytest.mark.parametrize("n_mc", [20, 50, 100, 200, 500])
 @pytest.mark.parametrize("alpha", [0.001, 0.01, 0.05, 0.1, 0.2, 0.5])
-# @pytest.mark.parametrize("alpha", [0.5])
 def test_parameter_tuning(dataset_data, n_mc, alpha, sweep_logger):
     """
     Test 1: Grid Search for Alpha and NMC.
